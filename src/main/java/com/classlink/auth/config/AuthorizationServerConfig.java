22e582dc0483c51c917b112990bc780d2e14cf75
leechaeil <53418675+leechaeil@users.noreply.github.com>
2023-01-06 12:58:11 +0900

    배포테스트 단일 인스턴스 2 순환참조 오류 제거 1

--- src/main/java/com/classlink/auth/config/AuthorizationServerConfig.java
+++ src/main/java/com/classlink/auth/config/AuthorizationServerConfig.java
@@ -1,66 +1,77 @@
 package com.classlink.auth.config;
 
+import java.util.Arrays;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerEndpointsConfiguration;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerSecurityConfiguration;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-import com.classlink.auth.service.login.CustomAuthenticationProvider;
-import com.classlink.auth.service.login.CustomWebAuthenticationDetailsSource;
-
 import lombok.RequiredArgsConstructor;
 
-@Configuration
-@Import(AuthorizationServerEndpointsConfiguration.class)
 @RequiredArgsConstructor
-public class AuthorizationServerConfig extends AuthorizationServerSecurityConfiguration {
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Value("${security.oauth2.jwt.signkey}")
+	private String signKey;
+
+	private final AuthenticationManager authenticationManager;
 
 	private final UserDetailsService userDetailsService;
-	
-	private final CustomWebAuthenticationDetailsSource authenticationDetailsSource;
-	
+
+	private final TokenStore tokenStore;
+
+	private final JwtAccessTokenConverter jwtAccessTokenConverter;
+
+	private final TokenEnhancer jwtTokenEnhancer;
+
 	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+		clients.inMemory().withClient("classlink")
+				// .secret(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("classlink!02$"))
+				.secret(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("classlink!02$"))
+				.authorizedGrantTypes("password", "refresh_token").scopes("client")
+				.refreshTokenValiditySeconds((24 * 60 * 60) * 30).accessTokenValiditySeconds(24 * 60 * 60)
+				// .scopes("client").refreshTokenValiditySeconds((24*60*60)*30).accessTokenValiditySeconds(10)
+				.and().withClient("appServer")
+				// .secret(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("appServer"))
+				.secret(PasswordEncoderFactories.createDelegatingPasswordEncoder().encode("appServer"))
+				.authorizedGrantTypes("client_credentials").scopes("server").accessTokenValiditySeconds(0);
+	}
+
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		TokenEnhancerChain tokenEnhancerChain = new TokenEnhancerChain();
+		tokenEnhancerChain.setTokenEnhancers(Arrays.asList(jwtTokenEnhancer, jwtAccessTokenConverter));
+
+		endpoints.tokenStore(tokenStore).accessTokenConverter(jwtAccessTokenConverter).tokenEnhancer(tokenEnhancerChain)
+				.authenticationManager(authenticationManager).userDetailsService(userDetailsService);
+	}
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()").checkTokenAccess("isAuthenticated()") // allow check token
+				.allowFormAuthenticationForClients();
+	}
+
 	@Bean
-	public AuthenticationManager authenticationManagerBean() throws Exception {
-		return super.authenticationManagerBean();
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		super.configure(http);
-		CorsConfigurationSource source = corsConfigurationSource();
-		http.addFilterBefore(new CorsFilter(source), ChannelProcessingFilter.class);
-		http.formLogin().authenticationDetailsSource(authenticationDetailsSource);
-	}
-
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.authenticationProvider(authProvider());
-	}
-	
-	//@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
 		return new BCryptPasswordEncoder();
-	}
-
-	@Bean
-	public DaoAuthenticationProvider authProvider() {
-		CustomAuthenticationProvider authProvider = new CustomAuthenticationProvider();
-		authProvider.setUserDetailsService(userDetailsService);
-		//authProvider.setPasswordEncoder(bCryptPasswordEncoder());
-		return authProvider;
 	}
 
 	private CorsConfigurationSource corsConfigurationSource() {
